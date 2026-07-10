dcce7211e97c8984657c884ff485e7588b0f471f
Madhukar Tukaatu <madhukar@tukaatu.com>
2026-07-10 14:44:43 +0545

    Allow production frontend CORS

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,23 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'http://127.0.0.1:3000'],
+
+    'allowed_origins' => [
+        'https://tukaatuexpress.com',
+        'https://www.tukaatuexpress.com',
+        'http://localhost:3000',
+    ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => true,
+
+    'supports_credentials' => false,
 ];

73a8ff4dbde02ea822c354b6e2ed320e3ce33673
Madhukar Tukaatu <madhukar@tukaatu.com>
2026-09-01 01:09:55 +0545

    udpated permissio

--- modules/Staff/routes/api.php
+++ modules/Staff/routes/api.php
@@ -7,6 +7,15 @@ use Modules\Staff\Http\Controllers\StaffController;
 |--------------------------------------------------------------------------
 | Admin Staff Routes
 |--------------------------------------------------------------------------
+|
+| Permission convention:
+|
+| staff.view
+| staff.create
+| staff.update
+| staff.delete
+| staff.toggle
+|
 */
 
 Route::prefix('v1/admin')
@@ -20,15 +29,41 @@ Route::prefix('v1/admin')
             |--------------------------------------------------------------------------
             | Staff
             |--------------------------------------------------------------------------
-            | Auto generated permissions:
-            | users.view
-            | users.manage
             */
 
-            Route::get('staff', [StaffController::class, 'index'])
-                ->name('staff.index');
+            Route::get('staff', [
+                StaffController::class,
+                'index',
+            ])->name('staff.index');
 
-            Route::post('staff', [StaffController::class, 'store'])
-                ->name('staff.store');
+            Route::post('staff', [
+                StaffController::class,
+                'store',
+            ])->name('staff.store');
+
+            Route::get('staff/{staff}', [
+                StaffController::class,
+                'show',
+            ])->name('staff.show');
+
+            Route::put('staff/{staff}', [
+                StaffController::class,
+                'update',
+            ])->name('staff.update');
+
+            Route::patch('staff/{staff}', [
+                StaffController::class,
+                'update',
+            ])->name('staff.update.patch');
+
+            Route::delete('staff/{staff}', [
+                StaffController::class,
+                'destroy',
+            ])->name('staff.destroy');
+
+            Route::patch('staff/{staff}/toggle', [
+                StaffController::class,
+                'toggle',
+            ])->name('staff.toggle');
         });
     });
